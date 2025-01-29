917700871fbf8022d95115095ab8fa2e4eb95803
koustavbera <thekoustavofficial@gmail.com>
2025-01-29 10:40:34 +0530

    update

---
+++ CSW_2/LAB_3/interface1.java
@@ -0,0 +1,31 @@
+package CSW_2.LAB_3;
+
+interface Add {
+
+    int add(int a, int b);
+}
+
+interface Sub {
+
+    int sub(int a, int b);
+}
+
+class calculation implements Add, Sub {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
+
+public class interface1 {
+
+    public static void main(String[] args) {
+        calculation c = new calculation();
+        System.out.println(c.add(9, 5));
+        System.out.println(c.sub(9, 5));
+    }
+}
