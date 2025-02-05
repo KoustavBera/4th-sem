5fffae62ea635758cc4af636348f7f7e6a325481
koustavbera <thekoustavofficial@gmail.com>
2025-02-05 09:20:14 +0530

    update

---
+++ CSW_2/LAB_6/Generic_Method1.java
@@ -0,0 +1,14 @@
+package CSW_2.LAB_6;
+
+public class Generic_Method1 {
+
+    static <T> void genericShow(T element) {
+        System.out.println(element.getClass().getName() + "=" + element);
+    }
+
+    public static void main(String[] args) {
+        genericShow(15);
+        genericShow("Hello");
+        genericShow(17.71);
+    }
+}
