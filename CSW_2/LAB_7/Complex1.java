890febb25333808337c268962cc7d137c620b3cc
Koustav Bera <143155312+KoustavBera@users.noreply.github.com>
2025-03-04 11:53:00 +0530

    Create Complex1.java

---
+++ CSW_2/LAB_7/Complex1.java
@@ -0,0 +1,16 @@
+class Complex1{
+	private double re,im;
+	public Complex1(double re,double im) {
+		this.re=re;
+		this.im=im;
+	}
+}
+public class ToString1 {
+
+	public static void main(String[] args) {
+		Complex1 c=new Complex1(5,7);
+		System.out.println(c);
+	}
+
+}
+//Complex12@6504e3b2
