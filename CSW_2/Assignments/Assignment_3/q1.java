3a724543c9f407acb261da73173e044d148c2ba2
swetanjana <142211747+swetanjana@users.noreply.github.com>
2025-03-23 14:22:04 +0530

    Update q1.java

--- CSW_2/Assignments/Assignment_3/q1.java
+++ CSW_2/Assignments/Assignment_3/q1.java
@@ -1,23 +1,37 @@
-package CSW_2.Assignments.Assignment_3;
-
-public class q1 {
-
-    static class NoNumericCharException extends Exception {
-
-        public NoNumericCharException(String s) {
-            super(s);
-        }
-    }
-
-    public static void main(String[] args) throws NoNumericCharException {
-        String s = "stn";
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c >= 48 && c <= 57) {
-                throw new NoNumericCharException("No Number allowed in String");
+// You are given a string containing alphanumeric characters, and your task is to design a 
+// Java program that extracts and displays the numeric characters from the given string. If 
+// no numeric characters are present, the program should display an appropriate message 
+// indicating their absence. Additionally, if the input string is null or empty, the program 
+// must throw a NullPointerException with a meaningful error message. 
+import java.util.Scanner;
+class q1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an alphanumeric string: ");
+        String s = sc.nextLine();
+        try{
+            String n=extractno(s);
+            if (n.length() == 0) {
+                System.out.println("No numeric characters found in the input string.");
             } else {
-                System.out.println("Status:OK");
+                System.out.println("Extracted numbers: " + n);
             }
         }
+        catch(NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+       
+    }
+    public static String extractno(String s){
+       if(s.length()==0){
+          throw new NullPointerException("String must not be empty");
+       }
+       String n="";
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='0'&& s.charAt(i)<='9')
+            n+=s.charAt(i);
+        }
+       return  n;
     }
 }
+        
