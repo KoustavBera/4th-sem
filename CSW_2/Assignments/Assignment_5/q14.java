e9ea4bc463bd2ccf7ca9f6a75561ba9c1e9b8f3c
KoustavBera <thekoustavofficial@gmail.com>
2025-04-01 23:20:30 +0530

    update

---
+++ CSW_2/Assignments/Assignment_5/q14.java
@@ -0,0 +1,38 @@
+package CSW_2.Assignments.Assignment_5;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
+
+public class q14 {
+
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter the source path");
+		Scanner sc = new Scanner(System.in);
+		Path source = Path.of(sc.nextLine());
+		System.out.println("Enter the destination path");
+		Path dest = Path.of(sc.nextLine());
+		if (!Files.exists(source)) {
+			System.out.println("File not found");
+			return;
+		}
+		if (Files.exists(dest)) {
+			System.out.println("Destination file already exist! Overwrite?(y/n)");
+			String choice = sc.next();
+			if (!choice.equalsIgnoreCase("Y")) {
+				System.out.println("Operation cancelled");
+				return;
+			}
+			try {
+				Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+				System.out.println("Files copied successfully!");
+			} catch (IOException e) {
+				System.out.println("error:" + e);
+			}
+
+		}
+	}
+}
